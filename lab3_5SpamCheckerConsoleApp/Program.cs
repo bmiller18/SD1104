13f384f303a20aa12b9d903934c9ae22d44ae56c
Brittany Miller <60929575+bmiller18@users.noreply.github.com>
2020-08-04 09:26:25 -0400

    Add files via upload

---
+++ lab3_5SpamCheckerConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace lab3_5SpamCheckerConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Updated and works but needs refactoring
+            // - More words in Blacklist
+            // - Resolve Uppercase/Lowercase issue with words
+            // - Use external db or flatfile that is easy to update without having to update the code
+            // Added 06.23.2020
+            Console.WriteLine("Hello World!");
+
+            string blackListWord = "boom";
+            bool isSpam = false;
+
+            string message = Console.ReadLine();
+
+            if (message.Contains(blackListWord))
+            {
+                isSpam = true;
+                Console.WriteLine("This message is spam!");
+            }
+            else
+            {
+                isSpam = false;
+                Console.WriteLine("This message is not spam.");
+            }
+        }
+    }
+}
---
+++ lab4_4ImprovedSpamChecker/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace lab4_4ImprovedSpamChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string[] blackList =
+            {
+                "buy", "viagra", "XXX", "free money",
+                "lifetime offer", "send money", "bank account",
+                "nigeria", "online pharmacy", "h8te", "meet girls"
+            };
+
+            string message = Console.ReadLine();
+            bool isSpam = false;
+            message = message.ToLower();
+            for (int i = 0; i < blackList.Length; i++)
+            {
+                if (message.Contains(blackList[i]))
+                {
+                    isSpam = true;
+                }
+            }
+
+            if(isSpam == true)
+            {
+                Console.WriteLine("The message contained spam.");
+            }
+            else
+            {
+                Console.WriteLine("This message is Ok.");
+            }
+        }
+    }
+}
